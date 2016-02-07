955cd8b54b7f986751cb5ab62eeabd090ef20444
Jessica <a71164@alunos.uminho.pt>
2016-02-07 01:12:57 +0000

    apply changes until Lesson 11 - thenewboston
    Note: How to use scanf properly. Use "&". (not necessary for arrays)
    --
    
    https://www.youtube.com/watch?v=hSHFjPvqFjw&list=PL6gx4Cwl9DGAKIXv8Yr6nhGJ9Vlcjyymq&index=11

--- FirstLessons/ola.c
+++ FirstLessons/ola.c
@@ -3,5 +3,19 @@
 
 int main(){
 	printf("Hello Alert!\a\n");
+
+	//Scanf
+	char username[10];
+	char countryname[10];
+	int age;
+
+	printf("What is your name?\n");
+	scanf("%s", username);
+	printf("Where are you from?\n");
+	scanf("%s", countryname);
+	printf("How old are you?\n");
+	scanf("%d", &age);
+
+	printf("\nWelcome %s ! We love %s. We also know you were born in %d\n", username, countryname, 2016-age);
 	return 0;
 }
