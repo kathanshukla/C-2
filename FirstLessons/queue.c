dbd522a873223e004ef512b3ccbd92a92dc3ec0f
Jessica <a71164@alunos.uminho.pt>
2016-02-18 22:09:27 +0000

    Add iniciation of queue implementantion (to be implemented more in the future.

---
+++ FirstLessons/queue.c
@@ -0,0 +1,19 @@
+// Queue Lista ligada FIFO
+
+Typedef struct Node{
+	int data;
+	Node * next;
+
+} *Nodo;
+
+Typedef struct Queue;
+
+Nodo inicia(){
+	Temp * = malloc(sizeof(struct Node));
+	Temp->first = Temp->last = NULL;
+	return Temp;
+}
+
+Void enqueue(){
+	
+}
