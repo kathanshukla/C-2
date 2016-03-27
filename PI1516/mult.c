04b2e7af5817ae493eb495f067e73681276f6e61
Jessica <a71164@alunos.uminho.pt>
2016-03-27 21:01:38 +0100

    add matrice multiplication 2x2 function and main

---
+++ PI1516/mult.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+
+void mult(int N, int M, int K, int r[N][M], int a[N][K], int b[K][M]){
+	
+	int i , j;
+	for(i=0;i<N;i++){
+	for(j=0;j<M;j++){
+
+		r[i][j] = a[i][0] * b[0][j] + a[i][1]*b[1][j];
+		}
+	}
+}
+
+
+int main(){
+
+	/*int a[2][2] ={  {1,1},  {2,1}};
+	int b[2][2] = { {1,2}, {1,1}};
+	*/
+	int a[2][2], b[2][2]; int N,M; N=2,M=2;
+	a[0][0]=1; a[0][1] = 1; a[1][0] = 2; a[1][1] = 1;
+	b[0][0]=1; b[0][1] = 2; b[1][0] = 1; b[1][1] = 1;
+	
+	int r[2][2];
+
+	int i,j;
+	//printing
+	for(i=0;i<2;++i){
+		for(j=0;j<2;++j){
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("  x  \n");
+	for(i=0;i<2;++i){
+		for(j=0;j<2;++j){
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+	printf("=");
+
+
+	
+
+	mult(2,2,2,r,a,b);
+	/*for(i=0;i<N;i++){
+		for(j=0;j<M;j++){
+
+			r[i][j] = a[i][0] * b[0][j] + a[i][1]*b[1][j];
+			}
+		}*/
+	//Problem solved, passed as parameters and working! TODO: more dimensions.
+			//This version is only for 2x2 matrices.
+
+	//printing
+	for(i=0;i<2;++i){
+		for(j=0;j<2;++j){
+			printf("%d\t",r[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 1;
+}
