2b8934378382c62875884467880ca19b479eebcb
Jessica <a71164@alunos.uminho.pt>
2016-04-26 17:35:39 +0100

    add p1.c primeira parte das 50 questões

---
+++ 50questoes/p1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	int soma = 0, numero = 0, maior = 0, maior2=0, nums=0;
+	int media = 0;
+	scanf("%d", &numero);
+
+	while(numero != 0){
+		soma+= numero;
+		if(numero > maior){
+			maior2 = maior;
+			maior = numero;
+		} 
+		if(numero > maior2 && numero < maior){
+			maior2 = numero;
+		}
+
+		nums++;
+		scanf("%d", &numero);
+	}
+
+	if(nums != 0) media = soma / nums;
+	printf("Soma = %d \n Média = %d \n Maior = %d\n Segundo Maior = %d\n Total numeros: %d\n\n", soma, media, maior, maior2, nums);
+
+	return 0;
+}
