5c6a29791275c5a5b48ba8199c0537e63b4f13af
Jessica <a71164@alunos.uminho.pt>
2016-05-13 19:41:37 +0100

    adiciona exo 2 e 3 do teste

--- PI1516/ExosTestes/PI1415-teste.c
+++ PI1516/ExosTestes/PI1415-teste.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct slist{
 	int valor;
@@ -51,9 +52,53 @@ char *strstr(char s1[], char s2[]){
 
 }
 
+//exo2
+int maxInd(int v[], int n){
+	int maior=0, maiorindice=0;
+	int i;
+	for(i=0; i<n;i++){
+		if(v[i] > maior){
+			maior = v[i];
+			maiorindice = i;
+		}
+	}
+	return maiorindice;
+}
+
+//Exo 3
+void concat3(LInt *a, LInt b){
+	LInt aux = *a;
+	if(a==NULL) a = &b;
+	while(aux->prox){
+		aux = aux->prox; 
+	}
+	aux->prox = b;
+}
+
+LInt concat(LInt a, LInt b){
+	LInt aux = a;
+	if(a==NULL) return b;
+	else{
+		while(aux->prox){
+			aux = aux->prox;
+		}
+		aux->prox=b;
+		return a;
+	}
+	
+}
+
+void printL(LInt a){
+	LInt aux = a;
+	while(aux){
+		printf("Valor %d \n", aux->valor);
+		aux=aux->prox;
+	}
+}
+
 int main(){
 
-
+//Testar exo 1
 	char s1[10] ="marianas";
 	char s2[4] ="ana";
 
@@ -61,7 +106,35 @@ int main(){
 
 	printf("%s  %s  %s\n ", s1, s2, o);
 
+//
+	//Testar exo2
+	int v[4] ={ 90, 4, 80, 2};
+	int in = maxInd(v, 4);
+	printf("O indice do maior valor no vetor v é %d cujo elemento é %d\n", in, v[in]);
+
+//Testar exo 3
+	printf("Antes do concat\n");
+	LInt a = (LInt)malloc(sizeof(struct slist));
+	a->valor = 10;
+	a->prox = (LInt)malloc(sizeof(struct slist));
+	a->prox->valor = 20;
+	a->prox->prox = NULL;
+
+	printL(a);
+
+	LInt b = (LInt)malloc(sizeof(struct slist));
+	b->valor = 30;
+	b->prox = (LInt)malloc(sizeof(struct slist));
+	b->prox->valor = 40;
+	b->prox->prox= NULL;
+	printL(b);
 	
+	concat3(&a, b);
+	
+	printf("Depois do concat\n");
+
+	//a = concat(a,b);
+	printL(a);
 
 	return 0;
 }
