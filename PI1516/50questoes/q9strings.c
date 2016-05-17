5cbc39da9615c0e99d7f7b4931b5071afff92b86
Jessica <a71164@alunos.uminho.pt>
2016-05-17 18:53:47 +0100

    submete q11 e q12 das 50 questoes
    Trabalhar com strings em C, strcmp e strcpy

--- PI1516/50questoes/q9strings.c
+++ PI1516/50questoes/q9strings.c
@@ -9,6 +9,7 @@ int strlens(char str[]){
 	return i;
 }
 
+//Q10
 //Concatena s2 a s1
 char* strcats(char s1[], char s2[]){
 	int i, j;
@@ -23,15 +24,72 @@ char* strcats(char s1[], char s2[]){
 	return s1;
 }
 
+/**
+*	copia a string source para dest retornando o valor desta ultima
+*/
+char* strcpyt(char *dest, char source[]){
+	int i, j=0;
+	for(i=0;source[i];i++){
+		dest[j]=source[i];
+		j++;
+	}
+	dest[j]='\0';
+	return dest;
+}
+
+//Q 12
+int strcmps(char s1[], char s2[]){
+
+	int i, j=0; int yes=0;
+
+	//Testa comprimentos das palavras primeiro.
+	if(strlen(s1)>strlen(s2)) return 1;
+	if(strlen(s1)<strlen(s2)) return -1;
+
+	for(i=0;s1[i];i++){
+		if(s1[i] > s2[j]){
+			return 1;
+		}
+		else if(s1[i] < s2[j]){
+			return -1;
+		}
+		else{
+			j++;
+			continue;
+		}
+		
+	}
+	return yes;
+}
+
 int main(){
 
 	char je[8] ="jessica";
 	char me[6] = "maria";
+	
+	/*
 	printf("Nomes: %s   %s   \n", je, me);
 	printf("Tamanho de je = %d \n", strlens(je));
 
 	
 	printf("Concatenando... %s \n", strcats(je, me));
+	*/
+	
+	//StrCpyt Teste Questao 11
+	
+	/*
+	printf("String me antes de copiar: %s\n", me);
+	strcpyt(me, je);
+	printf("String me depois de copiar: %s\n", me);
+	*/
 
+	/*
+
+	if('a' < 'b') printf("YES");
+	if('b' > 'a') printf("YES");
+	if('a' == 'a') printf("YES");
+	*/
+
+	printf("%d\n", strcmps("ana", "ano"));
 	return 0;
 }
