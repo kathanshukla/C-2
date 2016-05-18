fae8d46490914b5aa45cc0d5e78301c223fad37c
Jessica <a71164@alunos.uminho.pt>
2016-05-18 11:02:27 +0100

    submete funçao words parteB

--- PI1516/ExosTestes/PI1415-testeParteB.c
+++ PI1516/ExosTestes/PI1415-testeParteB.c
@@ -70,6 +70,41 @@ void unwords(StrList s, char t[], int N){
 	t[totalN]='\0';
 }
 
+
+int words(char t[], StrList *l){
+    int i = 0, npals = 0;
+	StrList temp=NULL, aux=NULL, root=NULL;
+	aux = malloc(sizeof(StrList));
+	root = aux;
+	while(t[i] != '\0'){
+
+		if(i == 0){
+			aux->string = &t[i];
+			npals++;
+			aux->prox=NULL;
+
+		}else{
+
+			if(t[i] == ' '){
+
+				t[i] = '\0';
+
+			}else if(t[i-1] == '\0'){
+				temp = malloc(sizeof(StrList));
+				temp->string = &t[i];
+				temp->prox=NULL;
+				aux->prox = temp;
+				aux = aux->prox;
+				npals++;
+			}
+		}
+		i++;
+	}
+	*l = root;
+	return npals;
+}
+
+
 void printStr(StrList a){
 	while(a){
 		printf("%s\n", a->string);
@@ -77,6 +112,7 @@ void printStr(StrList a){
 	}
 	printf("Fim da StrList.\n");
 }
+
 
 int main(){
 	//Exo 1
@@ -87,7 +123,6 @@ int main(){
 	for(i=0;i<100;i++){
 		vetor[i] = 0;
 	}
-
 	le100numeros(vetor);
 	i = maxInd(vetor, 100);
 	printf("O maior numero é o %d no indice %d \n", vetor[i], i);
@@ -99,12 +134,13 @@ int main(){
 	/*StrList sl = (StrList)malloc(sizeof(struct strlist));
 	sl->string = "Maria";
 	sl->prox = NULL;
-
 	printStr(sl);
 	unwords(sl, t, 3);
 	printf("Vetor char: %s\n", t);
 	*/
 	//Teste 2
+	
+	/*
 	StrList s = (StrList)malloc(sizeof(struct strlist));
 	s->string = "Maria";
 	s->prox = (StrList)malloc(sizeof(struct strlist));
@@ -114,6 +150,15 @@ int main(){
 	unwords(s, t, 18);
 	printf("Vetor char: %s\n", t);
 
-	//TODO investigar. Até 6 unwords(s,t,6) funciona, mas a partir de 7 imprime mal.
+	*/
+	char pal[90] = "Ma no na ni";
+	int nuu = 0;
+
+	StrList o = NULL;
+
+	nuu = words(pal, &o);
+	printStr(o);
+	printf("%d\n", nuu);
+
 	return 0;
 }
